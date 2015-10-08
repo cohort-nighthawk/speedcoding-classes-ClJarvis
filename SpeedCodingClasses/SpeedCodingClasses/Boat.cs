f4d7e59ec9cb84e1a0d02690e7108d26ebcf8709
Chris_Jarvis <clj@ollure.com>
2015-10-07 21:36:49 -0500

    inital files

---
+++ SpeedCodingClasses/SpeedCodingClasses/Boat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedCodingClasses
+{
+    public class Boat : Vehicle
+    {
+        public int NumOfWheels { get { return 0; }}
+
+        String Horn()
+        {
+            return "honk";
+        }
+      
+    }
+        
+}
